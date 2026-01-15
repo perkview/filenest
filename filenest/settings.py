520a50aee8ebf502e90456e0fa39ae9800c7f236
perkview <perkview.earn@gmail.com>
2026-01-15 09:59:00 +0500

    Update settings.py

--- filenest/settings.py
+++ filenest/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -158,3 +159,5 @@ CSRF_TRUSTED_ORIGINS = [
 
 ]
 
+
+load_dotenv(BASE_DIR / ".env")
